caced2e82aff892d99e7661e148e9c96257d70d7
bufang <1931297+budaobu@users.noreply.github.com>
2020-04-01 14:53:30 +0800

    Update nextdns_linkedip.js

--- Surge/Scripts/nextdns_linkedip.js
+++ Surge/Scripts/nextdns_linkedip.js
@@ -1,6 +1,16 @@
 //Script event auto linked ipv4 nextdns: network-change
+/*
+[General]
+# > NextDNS (https://nextdns.io/)
+always-real-ip = link-ip.nextdns.io
+[Host]
+# NextDNS (https://nextdns.io/)
+link-ip.nextdns.io = server:45.90.xx.xx
+# > NextDNS
+event network-changed script-path=Scripts/nextdns_linkedip.js
+*/
 
-$httpClient.post('https://link-ip.nextdns.io/10110/a9eae90f59dd3j88', function(error, response, data){
+$httpClient.post('https://link-ip.nextdns.io/xxxxx/xxxxxxxxxxx', function(error, response, data){
   if (error) {
 $notification.post('NEXT DNS ', 'Internet error','');
     $done({});
