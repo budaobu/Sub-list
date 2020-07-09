08bc5247da56d1bb3ad4a98feb337647d17f903f
bufang <1931297+budaobu@users.noreply.github.com>
2020-07-09 11:54:31 +0800

    Update Ssid2.js

--- Surge/Scripts/Ssid2.js
+++ Surge/Scripts/Ssid2.js
@@ -1,5 +1,5 @@
 const SSID = $network.wifi.ssid;
-const proxywifi = !$persistentStore.read("lkWifiSsids")?["jf-china","Jf-china"]:JSON.parse($persistentStore.read("lkWifiSsids"));
+const proxywifi = !$persistentStore.read("lkWifiSsids")?["cellular","cellular"]:JSON.parse($persistentStore.read("lkWifiSsids"));
 let res = proxywifi.some(val => val === (!!SSID ? SSID : "cellular"));
 let lkWifiLast = !$persistentStore.read("lkWifiLast")?"abcdefghijklmnopqrstuvwxyz":$persistentStore.read("lkWifiLast");
 if (lkWifiLast!=(!!SSID ? SSID : "cellular")){
