e71aef69760d7b4777d594dc8f5c139b0e4e9eaf
bufang <1931297+budaobu@users.noreply.github.com>
2020-07-09 11:44:03 +0800

    Update ssid.js

--- Surge/Scripts/ssid.js
+++ Surge/Scripts/ssid.js
@@ -9,7 +9,7 @@ PS:记得自己修改WIFI名称
 虽然设置SSID可以达到基本相同功能
 使用脚本,Surge不会被suspend
 Rewrite和Scripting依然有效
-*/
+
 
 var wifiname = $network.wifi.ssid;
 var proxywifi = ["cellular"];
@@ -30,4 +30,18 @@ for (var i = 0; i < proxywifi.length; i++) {
 	
 	
 };
+$done();
+*/
+
+var wifiname = $network.wifi.ssid;
+var proxywifi = "cellular";
+if (wifiname == proxywifi){
+	$surge.setOutboundMode("direct");
+	//$notification.post("Meeta_Remind","您目前处于WIFI-Proxy","Surge已自动变为直连模式");
+	
+}
+else{
+	$surge.setOutboundMode("rule");
+	//$notification.post("Meeta_Remind","Surge已自动变为规则模式","");
+}
 $done();
