c70fd1f1a6dfa7a99758bd60b9136fe0da8140e6
bufang <1931297+budaobu@users.noreply.github.com>
2020-12-11 10:17:15 +0800

    Update auto-policy.js

--- Surge/Scripts/auto-policy.js
+++ Surge/Scripts/auto-policy.js
@@ -26,7 +26,7 @@
 let config = {
   global_direct: "♲ 𝐃𝐢𝐫𝐞𝐜𝐭",
   global_proxy: "𝑷𝒓𝒐𝒙𝒚",
-  silence: false, // 是否静默运行，默认false
+  silence: true, // 是否静默运行，默认false
   cellular: "RULE", // 蜂窝数据下的模式，RULE代表规则模式，PROXY代表全局代理，DIRECT代表全局直连
   wifi: "RULE", // wifi下默认的模式
   all_direct: ["WRT32X", "WRT32X Extreme"], // 指定全局直连的wifi名字
